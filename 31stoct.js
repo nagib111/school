c6dff13e83e0c4999f1b7171e13ac94c1cfdb8b5
nagib111 <147293933+nagib111@users.noreply.github.com>
2023-11-02 22:18:49 +0600

    I am Nagib and I did this

---
+++ 31stoct.js
@@ -0,0 +1,23 @@
+if (grade >= 90) {
+    txt = `A`;
+  } else if (grade >= 85) {
+    txt = `A-`;
+  } else if (grade >= 80) {
+    txt = `B+`;
+  } else if (grade >= 75) {
+    txt = `B`;
+  } else if (grade >= 70) {
+    txt = `B-`;
+  } else if (grade >= 65) {
+    txt = `C+`;
+  } else if (grade >= 60) {
+    txt = `C`;
+  } else if (grade >= 55) {
+    txt = `C-`;
+  } else if (grade >= 50) {
+    txt = `D+`;
+  } else if (grade >= 45) {
+    txt = `D`;
+  } else {
+    txt = `F`;
+  }
